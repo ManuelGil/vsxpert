d8d3c8a0764c28eaab0b6fb05c6bea555ef66b8a
ManuelGil <atencion.ms@outlook.com>
2024-02-20 21:28:06 -0500

    ci: :green_heart: update deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig, passthroughImageService } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://vsxpert.tech/',
+  site: 'https://vsxpert.tech/',
   base: '/',
   image: {
     service: passthroughImageService()
